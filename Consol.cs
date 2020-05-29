c5958204f4950848195883012487f809dd95e646
Sergey <joker3839@gmail.com>
2020-05-29 22:02:03 +0300

    Class Console
    создание проекта, и класс консоль

---
+++ Consol.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console
+{
+   static public class Consol
+    {
+        static public void Copy() // копировать каталог 
+        {
+
+        }
+        public static void Show(string path) // показать каталог
+        {
+
+        }
+        public static void New(string path) // создать новый каталог
+        {
+
+        }
+        public static void Dir() // переход к каталогу
+        {
+
+        }
+        static public void Del() // удалить каталог
+        {
+
+        }
+        public static void Help() // показать все доступные команды
+        {
+
+        }
+    }
+}
