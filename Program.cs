b1f9a1446800aa9e677abe6e586833dd27aa1981
Sergey <joker3839@gmail.com>
2020-05-29 22:03:01 +0300

    1
    1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+using static System.Console;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
