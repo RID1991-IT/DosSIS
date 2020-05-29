32f9960b0ce4c81f216650ead4843bc6558aa35e
Sergey <joker3839@gmail.com>
2020-05-29 22:04:03 +0300

    2
    2

--- Consol.cs
+++ Consol.cs
@@ -4,31 +4,7 @@ using System.Text;
 
 namespace Console
 {
-   static public class Consol
+    class Consol
     {
-        static public void Copy() // копировать каталог 
-        {
-
-        }
-        public static void Show(string path) // показать каталог
-        {
-
-        }
-        public static void New(string path) // создать новый каталог
-        {
-
-        }
-        public static void Dir() // переход к каталогу
-        {
-
-        }
-        static public void Del() // удалить каталог
-        {
-
-        }
-        public static void Help() // показать все доступные команды
-        {
-
-        }
     }
 }
