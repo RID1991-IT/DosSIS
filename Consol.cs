6f2d9cd4d9099c6a4331c5c1528fda7704d58241
Sergey <joker3839@gmail.com>
2020-05-30 15:52:47 +0300

    Console

--- Consol.cs
+++ Consol.cs
@@ -1,30 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using static System.Console;
 
 namespace Console
 {
     class Consol
     {
-        static void Copy() 
+        public static void CopyFile(string path, string newPath)//копирование файла
         {
-        
-        } 
-        static void Del() 
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                File.Copy(path, newPath, true);
+            }
+
+        }
+        public static void DelFile(string path) // Удаление файла
         {
-        
-        } 
-        static void Show() 
-        { 
-        
-        } 
-        static void Help() 
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                File.Delete(path);
+            }
+        }
+
+        public static string Show(string dirName)//показ каталогов и файлов
         {
-        
-        } 
-        static void Muve() 
+            if (Directory.Exists(dirName))
+            {
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string dir in dirs)
+                {
+                    ForegroundColor = ConsoleColor.Green;
+                    WriteLine("Подкаталоги:");
+                    WriteLine(dir);
+                }
+                WriteLine();
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string dir in files)
+                {
+                    ForegroundColor = ConsoleColor.Red;
+                    WriteLine("Файлы:");
+                    WriteLine(dir);
+                }
+                return dirName;
+            }
+            else
+            {
+                return "Wrong way";
+            }
+
+        }
+        public static void Help()
         {
-        
-        } 
+
+        }
+        public static void MoveFile(string path, string newPath) // Перемещение файла
+        {
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                File.Move(path, newPath);
+            }
+        }
+
+        public static void CreateFile(string path) // создание файла
+        {
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    
+                }
+            }
+        }
+        public static void CreateDirectory(string path, string subpath)//создание каталога
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            dirInfo.CreateSubdirectory(subpath);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 using static System.Console;
 
@@ -9,7 +10,48 @@ namespace Console
     {
         static void Main()
         {
+            string path, newpath, pathO, newPathDirectory, temp, subpath;
+           
             
+            
+            
+            
+            
+            
+            
+            
+            
+            //string slash;
+            //List<string> steps = new List<string>();
+            ////Реализация перехода по каталогам
+            //slash = @"\";
+            //pathO = @"E:\";
+            //steps.Add(pathO);
+            //Consol.Show(pathO);
+            //WriteLine("Введите каталог в какой вы хотите перейти");
+            //newPathDirectory = ReadLine();
+            // = pathO + newPathDirectory + slash;
+            //steps.Add(pathO);
+            //Consol.Show(pathO);
+            /////////////////////////////////////////////////////
+
+            //steps.IndexOf(pathO, 0);
+            //newpath =pathO+"hta.txt";
+            //WriteLine("Введите название файла");
+            //newpath = ReadLine();
+            path = @"E:\1\123.doc";
+            //subpath  = @"program\avalon";
+            //WriteLine("Выберете диск");
+            //pathO = ReadLine();
+            //Consol.Show(pathO);
+            //WriteLine("Введите путь файла");
+            //path = ReadLine();
+
+            //newpath = pathO + newpath;
+            //Consol.CopyFile(path, newpath);
+            //Consol.Del(path);
+            //Consol.CreateDirectory(path, subpath);
+            Consol.CreateFile(path);
         }
     }
 }
