0327482dc694047554cc491c2a626b13c1cf1e59
Sergey <joker3839@gmail.com>
2020-05-30 13:06:15 +0300

    Fix

--- Consol.cs
+++ Consol.cs
@@ -6,5 +6,25 @@ namespace Console
 {
     class Consol
     {
+        static void Copy() 
+        {
+        
+        } 
+        static void Del() 
+        {
+        
+        } 
+        static void Show() 
+        { 
+        
+        } 
+        static void Help() 
+        {
+        
+        } 
+        static void Muve() 
+        {
+        
+        } 
     }
 }
